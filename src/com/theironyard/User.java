d4fcab8e7f5e6911844f59b83d1165f4103d3824
Ben <bhturne@g.clemson.edu>
2016-06-08 10:46:30 -0400

    Initial Commit

---
+++ src/com/theironyard/User.java
@@ -0,0 +1,14 @@
+package com.theironyard;
+
+/**
+ * Created by Ben on 6/8/16.
+ */
+public class User {
+    String name;
+    String password;
+
+    public User(String password, String name) {
+        this.password = password;
+        this.name = name;
+    }
+}
