cd726ff4ffe26b12da039b1c218b8b239ded2b70
Ben <bhturne@g.clemson.edu>
2016-06-09 12:08:14 -0400

    add deletion function

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -41,10 +41,18 @@ public class Main {
                             subset.add(msg);
                         }
                     }
+
+                    Message parentMsg = null;
+                    if (replyId>= 0) {
+                        parentMsg= messages.get(replyId);
+                    }
+
                     HashMap m = new HashMap<>();
+                    m.put("message", parentMsg);
                     m.put("messages", subset);
                     m.put("username", username);
                     m.put("replyId", replyId);
+                    m.put("isMe", parentMsg != null && username != null && parentMsg.author.equals(username));
                     return new ModelAndView(m, "home.html");
                 },
                 new MustacheTemplateEngine()
@@ -102,6 +110,40 @@ public class Main {
 
 
                     response.redirect(request.headers("Referer"));
+                    return "";
+                }
+        );
+
+
+        Spark.post(
+                "/delete-message",
+                (request, response) -> {
+
+                    Session session = request.session();
+                    String username = session.attribute("username");
+                    if (username == null) {
+                        throw new Exception("Not logged in");
+                    }
+
+
+
+                    int id = Integer.valueOf(request.queryParams("id"));
+                    Message m = messages.get(id);
+                    if (!m.author.equals(username)) {
+                        throw new Exception("You can't delete this");
+                    }
+
+
+                    messages.remove(id);
+                    //resets ids
+                    int index = 0;
+                    for (Message msg: messages) {
+                        msg.id = index;
+                        index++;
+                    }
+
+
+                    response.redirect("/");
                     return "";
                 }
         );
